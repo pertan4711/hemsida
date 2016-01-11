79fe0c4c5b7a627f1de3a646409c7b345832c80c
Per Stensland <pertan4711@hotmail.com>
2016-01-11 20:51:51 +0100

    Resor tillagda

---
+++ WebApplication1/Controllers/ResaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ResaController : Controller
+    {
+        // GET: Resa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Resa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Resa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resa/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Resa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Resa/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Resa/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Resa/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Resa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Drawing;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Resa
+    {
+        public int ResaID { get; set; }
+
+        [Required()]
+        [Display(Name="Resmål")]
+        public string ResMal { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime Slut { get; set; }
+
+        public string Berattelse { get; set; }
+
+        public List<Image> Bilder { get; set; }
+    }
+}
