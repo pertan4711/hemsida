f7b9443673cb8ab0a096ecd7810e42f71d018799
Per Stensland <pertan4711@hotmail.com>
2020-10-09 18:08:56 +0200

    asp.net core nytt projekt

---
+++ Hemsida/Controllers/AppController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemsida.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuyPics()
+        {
+            ViewBag.Title = "Fiskar, fiskar, fiskar...";
+
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact us";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About us";
+
+            return View();
+        }
+    }
+}
---
+++ Hemsida/wwwroot/js/BuyPics.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    //alert("Hej hopp");
+    console.log("Hej hopp");
+
+    //var theForm = document.getElementById("theForm");
+    //theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
+
+
+    //var button = document.getElementById("buyButton");
+    //button.addEventListener("click", function () {
+    //    console.log("Buying item");
+    //});
+    var button = $("#buyButton");
+    button.on("click", () => {
+        console.log("Buying item");
+    })
+
+    //var productInfo = document.getElementsByClassName("product-props");
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", () => {
+        $popupForm.fadeToggle(250);
+    });
+});
